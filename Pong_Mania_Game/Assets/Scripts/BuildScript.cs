4f492c2277516e19962e36bea8abd27c4dccf6e1
Alim.Wooden <alimwooden@gmail.com>
2021-03-15 19:25:40 -0500

    adding one change

---
+++ Pong_Mania_Game/Assets/Scripts/BuildScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Adding Random Comment
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
